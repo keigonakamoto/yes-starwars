801be6154e36f38fbab0c655522a8442eea75ce3
DESKTOP-2FOS94E\仲本和子 <zhongbenhuiwu@gmail.com>
2021-11-18 16:11:44 +0900

    初コミット

---
+++ darth vader/script.js
@@ -0,0 +1,21 @@
+
+
+$(function() {
+
+    $(window).on('scroll', function() {
+      // idがq-11の要素の画面最上部からの距離を取得
+      let s1 = $('#s1').offset().top;
+  
+      // 画面の高さ
+      let wh = $(window).height();
+  
+      // $(window).scrollTop()は現在のスクロール位置
+      if (s1 <= $(window).scrollTop() + wh) {
+        $('.vader').removeClass('hide2');
+      } else {
+        $('.vader').addClass('hide2');
+      }
+      
+    })
+
+})
